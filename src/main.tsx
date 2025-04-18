babe48de987ca8964ef65eb1324133a35859fb18
Eric Dolo <lesetjadolo@gmail.com>
2025-04-18 08:54:13 +0200

    added redirection of url

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
 
+if (window.location.hostname === 'dolodynamicsolutions.com') {
+    window.location.href = 'https://www.dolodynamicsolutions.com';
+  }
+
 createRoot(document.getElementById("root")!).render(<App />);
