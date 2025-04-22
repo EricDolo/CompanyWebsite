31682c13c3869e7a953c21d914b11e3983f5be7b
Eric Dolo <lesetjadolo@gmail.com>
2025-04-22 11:33:21 +0200

    .

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App'
 import './styles/index.css'
 
 if (window.location.hostname === 'dolodynamicsolutions.com') {
-    window.location.href = 'https://www.dolodynamicsolutions.com';
+    window.location.href = 'https://www.dolodynamicsolutions.com/';
   }
 
 createRoot(document.getElementById("root")!).render(<App />);
